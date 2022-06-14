161b343b2325c168669707bbac9dd6e0c065855d
Leonamin <yism810@gmail.com>
2022-06-14 19:44:25 +0900

    delete getting community id

--- lib/model/Community.dart
+++ lib/model/Community.dart
@@ -2,20 +2,17 @@ import 'package:cuteshrew/model/Post.dart';
 import 'dart:convert';
 
 class Community {
-  final int communityId;
   final String communityName;
   final String communityShowName;
   List<Post> latestPostingList;
 
   Community(
-      {required this.communityId,
-      required this.communityShowName,
+      {required this.communityShowName,
       required this.communityName,
       required this.latestPostingList});
 
   factory Community.fromJson(Map<String, dynamic> json) {
     return Community(
-        communityId: json['id'],
         communityName: json['name'],
         communityShowName: json['showname'],
         latestPostingList: List<Post>.from(
