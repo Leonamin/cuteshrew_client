83cb59aa6bc98ca9a8ba5299e3799a48cf6254e3
Leonamin <yism810@gmail.com>
2022-07-14 23:04:51 +0900

    add no page animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cuteshrew/helpers/no_transition_builder.dart';
 import 'package:cuteshrew/layout/main_layout.dart';
 import 'package:cuteshrew/provider/login_provider.dart';
 import 'package:cuteshrew/provider/page_notifier.dart';
@@ -6,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:get_it/get_it.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   usePathUrlStrategy();
@@ -33,6 +34,17 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
           textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
               .apply(bodyColor: Colors.black),
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: kIsWeb
+                ? {
+                    // No animations for every OS if the app running on the web
+                    for (final platform in TargetPlatform.values)
+                      platform: const NoTransitionsBuilder(),
+                  }
+                : const {
+                    // handel other platforms you are targeting
+                  },
+          ),
         ),
         home: MainLayout());
   }
