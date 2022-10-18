375585c987c56bc7ea80279dd2cf33a0fb833517
Leonamin <yism810@gmail.com>
2022-10-18 22:48:25 +0900

    삭제 - 안쓰는 코드 삭제

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,8 +1,4 @@
-import 'package:cuteshrew/pages/deprecated/auth_screen_page.dart';
-import 'package:cuteshrew/pages/deprecated/deprecated_community_page.dart';
 import 'package:cuteshrew/pages/error_page.dart';
-import 'package:cuteshrew/pages/deprecated/post_editor_page.dart';
-import 'package:cuteshrew/pages/deprecated/posting_page.dart';
 import 'package:cuteshrew/routing/routes.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +6,14 @@ Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     // case CommunityHomePageRoute:
     //   return _getPageRoute(HomePage());
-    case CommunityPageRoute:
-      return _getPageRoute(CommunityPage(settings.arguments as dynamic));
-    case PostingPageRoute:
-      return _getPageRoute(PostingPage(settings.arguments as dynamic));
-    case PostEditorPageRoute:
-      return _getPageRoute(PostEditorPage(settings.arguments as dynamic));
-    case AuthenticationPageRoute:
-      return _getPageRoute(AuthScreenPage());
+    // case CommunityPageRoute:
+    //   return _getPageRoute(CommunityPage(settings.arguments as dynamic));
+    // case PostingPageRoute:
+    //   return _getPageRoute(PostingPage(settings.arguments as dynamic));
+    // case PostEditorPageRoute:
+    //   return _getPageRoute(PostEditorPage(settings.arguments as dynamic));
+    // case AuthenticationPageRoute:
+    //   return _getPageRoute(AuthScreenPage());
 
     default:
       return _getPageRoute(PageNotFound());
