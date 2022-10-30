2926f8fc3cb298e17886550159e9dda9d4e2b1d8
Leonamin <yism810@gmail.com>
2022-10-30 18:47:07 +0900

    추가 - 원형 버튼, 리스트 버튼 추가

---
+++ lib/widgets/list_button.dart
@@ -0,0 +1,67 @@
+import 'package:cuteshrew/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class ButtonProperties {
+  int page;
+  Color color;
+
+  ButtonProperties({required this.page, this.color = Colors.black});
+}
+
+class ListButtonProperties {
+  int id;
+  Color color;
+  VoidCallback onPressed;
+
+  ListButtonProperties({
+    required this.id,
+    required this.color,
+    required this.onPressed,
+  });
+}
+
+class ListButton extends StatefulWidget {
+  ListButton({
+    super.key,
+    required this.itemCount,
+    this.backgroundColor,
+    this.selectedColor,
+    this.maxHeight,
+    required this.onPressed,
+    required this.propertyList,
+    this.selectedIndex,
+  });
+  Color? backgroundColor;
+  Color? selectedColor;
+  VoidCallback onPressed;
+  double? maxHeight;
+  int itemCount;
+  List<ListButtonProperties> propertyList;
+  int? selectedIndex = 0;
+
+  @override
+  State<ListButton> createState() => _ListButtonState();
+}
+
+class _ListButtonState extends State<ListButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: widget.propertyList
+          .map((item) => CircleElevatedButton(
+                color: item.id == widget.selectedIndex
+                    ? widget.selectedColor ?? Colors.blue
+                    : Colors.grey,
+                onPressed: () {
+                  setState(() {
+                    widget.selectedIndex = item.id;
+                  });
+                  item.onPressed();
+                },
+                child: Text(item.id.toString()),
+              ))
+          .toList(),
+    );
+  }
+}
