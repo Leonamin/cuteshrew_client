681e42434e2ff22a4ef8a10c3ee718056bb3c034
Leonamin <yism810@gmail.com>
2022-11-05 15:45:45 +0900

    추가 - 댓글 관련 기능 버튼 추가

--- lib/pages/posting/comment_card.dart
+++ lib/pages/posting/comment_card.dart
@@ -1,5 +1,6 @@
 import 'package:cuteshrew/constants/style.dart';
 import 'package:cuteshrew/models/comment_detail.dart';
+import 'package:cuteshrew/states/login_state.dart';
 import 'package:cuteshrew/utils/utils.dart';
 import 'package:flutter/material.dart';
 
@@ -8,59 +9,152 @@ class CommentCard extends StatelessWidget {
 
   CommentDetail comment;
 
+  int voteCount = 0;
+
+  SnackBar _makeSnackBar(String content, [Color? backgroundColor]) {
+    return SnackBar(
+      content: Text(content),
+      backgroundColor: backgroundColor,
+    );
+  }
+
+  Card _header() {
+    return Card(
+      color: Colors.grey[300],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // 프로필 사진, 닉네임
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30)),
+                child: Container(
+                    padding: const EdgeInsets.all(2),
+                    margin: const EdgeInsets.all(2),
+                    child: const CircleAvatar(
+                        backgroundColor: light,
+                        child: Icon(
+                          Icons.person_outline,
+                          color: dark,
+                        ))),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                comment.userInfo.name,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              )
+            ],
+          ),
+          // 시간
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Text(
+              Utils.formatTimeStamp(comment.createdAt),
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 14),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  String formatVoteCount(int voteCount) {
+    final trillion = 1000000000000;
+    final billion = 1000000000;
+    final million = 1000000;
+    final kilo = 1000;
+
+    if (voteCount >= trillion) {
+      return "999+b";
+    }
+    if (voteCount >= billion) {
+      return "${(voteCount / billion).toStringAsFixed(1)}b";
+    }
+    if (voteCount >= million) {
+      return "${(voteCount / million).toStringAsFixed(1)}m";
+    }
+    if (voteCount >= kilo) {
+      return "${(voteCount / kilo).toStringAsFixed(1)}k";
+    }
+
+    return voteCount.toString();
+  }
+
+  void _deleteComment(LoginState loginState) {
+    if (loginState is AuthorizedState) {
+    } else {
+      // ScaffoldMessenger.of(context).showSnackBar(_makeSnackBar("로그인이 필요함"));
+    }
+  }
+
+  Widget _bottom() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        TextButton.icon(
+            style: TextButton.styleFrom(foregroundColor: Colors.green),
+            onPressed: () {},
+            icon: const Icon(Icons.thumb_up_alt_rounded),
+            label: Text(formatVoteCount(voteCount))),
+        TextButton.icon(
+            style: TextButton.styleFrom(foregroundColor: Colors.red),
+            onPressed: () {},
+            icon: const Icon(
+              Icons.thumb_down_rounded,
+            ),
+            label: Text(formatVoteCount(voteCount))),
+        TextButton.icon(
+          style: TextButton.styleFrom(foregroundColor: Colors.grey),
+          onPressed: () {},
+          icon: const Icon(
+            Icons.add_comment,
+          ),
+          label: const Text("답글"),
+        ),
+        TextButton.icon(
+          style: TextButton.styleFrom(foregroundColor: Colors.grey),
+          onPressed: () {},
+          icon: const Icon(
+            Icons.share,
+          ),
+          label: const Text("공유"),
+        ),
+        TextButton.icon(
+          style: TextButton.styleFrom(foregroundColor: Colors.grey),
+          onPressed: () {},
+          icon: const Icon(
+            Icons.edit,
+          ),
+          label: const Text("수정"),
+        ),
+        TextButton.icon(
+          style: TextButton.styleFrom(foregroundColor: Colors.red),
+          onPressed: () {},
+          icon: const Icon(
+            Icons.delete,
+          ),
+          label: const Text("삭제"),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Card(
-          color: Colors.grey[300],
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // 프로필 사진, 닉네임
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30)),
-                    child: Container(
-                        padding: const EdgeInsets.all(2),
-                        margin: const EdgeInsets.all(2),
-                        child: const CircleAvatar(
-                            backgroundColor: light,
-                            child: Icon(
-                              Icons.person_outline,
-                              color: dark,
-                            ))),
-                  ),
-                  const SizedBox(
-                    width: 4,
-                  ),
-                  Text(
-                    comment.userInfo.name,
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16),
-                  )
-                ],
-              ),
-              // 시간
-              Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Text(
-                  Utils.formatTimeStamp(comment.createdAt),
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(0.5), fontSize: 14),
-                ),
-              )
-            ],
-          ),
-        ),
+        _header(),
         const SizedBox(
           height: 4,
         ),
@@ -71,6 +165,10 @@ class CommentCard extends StatelessWidget {
             style: const TextStyle(color: Colors.black),
           ),
         ),
+        const SizedBox(
+          height: 4,
+        ),
+        _bottom(),
       ],
     );
   }
