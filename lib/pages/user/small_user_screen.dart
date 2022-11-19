fe6090864c39cbbf203aa00e0099df9887e2350c
Leonamin <yism810@gmail.com>
2022-11-19 11:58:36 +0900

    추가 - UserPage 분기 생성

--- lib/pages/user/small_user_screen.dart
+++ lib/pages/user/small_user_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cuteshrew/api/cuteshrew_api_client.dart';
 import 'package:cuteshrew/providers/user_page_provider.dart';
-import 'package:cuteshrew/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,32 +15,64 @@ class SmallUserScreen extends StatefulWidget {
 class _SmallUserScreenState extends State<SmallUserScreen> {
   @override
   Widget build(BuildContext context) {
-    print("asd");
     if (widget.userId == null && widget.userName == null) {
       // 유저 없음 처리
-      return Container();
+      return NotFoundSmallUserScreen();
     } else {
-      return ChangeNotifierProvider(
-        create: (context) {
-          final provider =
-              UserPageProvider(api: context.read<CuteshrewApiClient>());
-          widget.userId != null
-              ? provider.fetchPostings(userId: widget.userId)
-              : provider.fetchPostings(userName: widget.userName);
-          return provider;
-        },
-        child: Consumer<UserPageProvider>(
-          builder: (context, provider, child) {
-            final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
-            return ListView.builder(
-              itemCount: provider.userPostings.length,
-              itemBuilder: (context, index) {
-                return Text(provider.userPostings[index].title);
-              },
-            );
-          },
-        ),
-      );
+      return LoadedSmallUserScreen();
     }
+  }
+}
+
+class NotFoundSmallUserScreen extends StatelessWidget {
+  const NotFoundSmallUserScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text("해당 사용자는 존재하지 않습니다."),
+        const SizedBox(
+          height: 8,
+        ),
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("뒤로가기")),
+      ],
+    );
+  }
+}
+
+class LoadedSmallUserScreen extends StatefulWidget {
+  int? userId;
+  String? userName;
+  LoadedSmallUserScreen({super.key, this.userId, this.userName});
+
+  @override
+  State<LoadedSmallUserScreen> createState() => _LoadedSmallUserScreenState();
+}
+
+class _LoadedSmallUserScreenState extends State<LoadedSmallUserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) {
+        final provider =
+            UserPageProvider(api: context.read<CuteshrewApiClient>());
+        widget.userId != null
+            ? provider.fetchPostings(userId: widget.userId)
+            : provider.fetchPostings(userName: widget.userName);
+        return provider;
+      },
+      child: Consumer<UserPageProvider>(
+        builder: (context, provider, child) {
+          return Container();
+        },
+      ),
+    );
   }
 }
