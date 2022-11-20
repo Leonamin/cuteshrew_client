fcf045e3dd2495bcf63a4e237b4a4f56c60f2196
Leonamin <yism810@gmail.com>
2022-11-20 23:42:41 +0900

    추가 - 게시글 목록 불러오기 만들고 아직 더보기는 버그있다

--- lib/pages/user/small_user_screen.dart
+++ lib/pages/user/small_user_screen.dart
@@ -1,4 +1,7 @@
 import 'package:cuteshrew/api/cuteshrew_api_client.dart';
+import 'package:cuteshrew/model/models.dart';
+import 'package:cuteshrew/models/user_info.dart';
+import 'package:cuteshrew/pages/user/widget/tab_bar_delegate.dart';
 import 'package:cuteshrew/providers/user_page_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -19,7 +22,9 @@ class _SmallUserScreenState extends State<SmallUserScreen> {
       // 유저 없음 처리
       return NotFoundSmallUserScreen();
     } else {
-      return LoadedSmallUserScreen();
+      return LoadedSmallUserScreen(
+        userName: widget.userName,
+      );
     }
   }
 }
@@ -56,21 +61,307 @@ class LoadedSmallUserScreen extends StatefulWidget {
   State<LoadedSmallUserScreen> createState() => _LoadedSmallUserScreenState();
 }
 
-class _LoadedSmallUserScreenState extends State<LoadedSmallUserScreen> {
+class _LoadedSmallUserScreenState extends State<LoadedSmallUserScreen>
+    with TickerProviderStateMixin {
+  ScrollController? _mainScrollController;
+  TabController? _tabController;
+
+  bool lastStatus = true; // 이거 사실 안씀
+  double appBarHeight = 275;
+
+  // 앱바가 숨겨지려면 역방향으로 내려야해서 Offset이 늘어난다
+  bool get _isShrink {
+    return _mainScrollController != null &&
+        _mainScrollController!.hasClients &&
+        _mainScrollController!.offset > (appBarHeight - kToolbarHeight);
+  }
+
+  void _mainScrollListener() {
+    if (_isShrink != lastStatus) {
+      setState(() {
+        lastStatus = _isShrink;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _mainScrollController = ScrollController()
+      ..addListener(_mainScrollListener);
+    _tabController = TabController(initialIndex: 0, length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _mainScrollController?.removeListener(_mainScrollListener);
+    _mainScrollController?.dispose();
+    super.dispose();
+  }
+
+  // 앱바가 펴진 상태에서 보여줄 위젯
+  _userInfoWidget(UserInfo user) {
+    return SafeArea(
+        child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 48),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            // child: Image.network(
+            //   userImage,
+            //   fit: BoxFit.cover,
+            //   height: 100,
+            //   width: 100,
+            // ),
+            child: Container(
+              color: Colors.white,
+              child: Icon(Icons.person_outline_outlined, size: 100),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          user.name,
+          style: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(user.email),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          //user.profile.introduce
+          "Introduce",
+        ),
+      ],
+    ));
+  }
+
+  // 앱바가 줄여진 상태에서 보여줄 위젯
+  _shrinkUserInfoWidget(UserInfo user) {
+    return [
+      Padding(
+        padding: const EdgeInsets.only(left: 8, right: 12),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    user.name,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    user.email,
+                    style: const TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              // child: Image.network(
+              //   headerImage,
+              //   fit: BoxFit.cover,
+              //   height: 30,
+              //   width: 30,
+              // ),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  _appBar(UserInfo user) {
+    return SliverAppBar(
+      elevation: 0,
+      backgroundColor: Colors.grey.shade300,
+      pinned: true,
+      expandedHeight: appBarHeight,
+      flexibleSpace: FlexibleSpaceBar(
+        collapseMode: CollapseMode.parallax,
+        title: _isShrink ? Text("Profile") : null,
+        background: _userInfoWidget(user),
+      ),
+      actions: _isShrink ? _shrinkUserInfoWidget(user) : null,
+    );
+  }
+
+  _tabBar() {
+    return SliverPersistentHeader(
+      delegate: TabBarDelegate(tabcontroller: _tabController!, tabs: [
+        Tab(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            color: Colors.white,
+            child: const Text("게시글"),
+          ),
+        ),
+        Tab(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            color: Colors.white,
+            child: const Text("댓글"),
+          ),
+        ),
+      ]),
+      pinned: true,
+    );
+  }
+
+  commentCountToString(int count) {
+    if (count >= 100) {
+      return "99+";
+    } else {
+      return count.toString();
+    }
+  }
+
+  _postingItemRow(PostPreview posting) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: Text(
+              posting.title,
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                commentCountToString(10),
+                maxLines: 1,
+              ),
+              style: TextButton.styleFrom(
+                  minimumSize: const Size(60, 60),
+                  backgroundColor: Colors.grey.withOpacity(0.2))),
+        ],
+      ),
+    );
+  }
+
+  _makePostingListWidget(
+    PageStorageKey<String> key,
+    UserPageProvider provider,
+  ) {
+    // 로딩 중이면서 캐시가 없음
+    if (provider.isLoadingPosting && provider.userPostings.isEmpty) {
+      return const SizedBox(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    // 로딩 아닌데 캐시가 없음
+    if (!provider.isLoadingPosting && provider.userPostings.isEmpty) {
+      return const SizedBox(
+        height: 50,
+        child: Center(
+          child: Text("데이따 없음"),
+        ),
+      );
+    }
+
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Builder(
+        builder: (context) {
+          return CustomScrollView(
+            key: key,
+            // controller: controller, 이거 넣으면 sliver 효과 없어서 중첩 스크롤이 안된다.
+            slivers: [
+              SliverOverlapInjector(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              ),
+              SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                childCount: provider.userPostings.length + 1,
+                (context, index) {
+                  if (index < provider.userPostings.length) {
+                    return _postingItemRow(provider.userPostings[index]);
+                  }
+                  return InkWell(
+                    onTap: () {
+                      provider.fetchPostings(
+                          userName: widget.userName,
+                          nextPostId: provider.userPostings[index - 1].postId);
+                    },
+                    child: SizedBox(
+                      height: 50,
+                      child: Center(
+                          child: provider.isLoadingPosting
+                              ? const CircularProgressIndicator()
+                              : const Text("더보기")),
+                    ),
+                  );
+                },
+              ))
+            ],
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) {
         final provider =
             UserPageProvider(api: context.read<CuteshrewApiClient>());
-        widget.userId != null
-            ? provider.fetchPostings(userId: widget.userId)
-            : provider.fetchPostings(userName: widget.userName);
+        provider.fetchPostings(userName: widget.userName);
         return provider;
       },
       child: Consumer<UserPageProvider>(
         builder: (context, provider, child) {
-          return Container();
+          return Scaffold(
+            body: NestedScrollView(
+                controller: _mainScrollController,
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    _appBar(UserInfo(
+                        name: widget.userName ?? "null",
+                        email: widget.userName ?? "null")),
+                    SliverOverlapAbsorber(
+                      handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                          context),
+                      sliver: _tabBar(),
+                    ),
+                  ];
+                },
+                body: TabBarView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: _tabController,
+                  children: [
+                    _makePostingListWidget(
+                        PageStorageKey<String>("1"), provider),
+                    Container()
+                  ],
+                )),
+          );
         },
       ),
     );
