feb9ed6c9193f2b1dd3ed9826c17f80919fb0a46
Leonamin <yism810@gmail.com>
2022-12-23 19:08:48 +0900

    추가 - 커뮤니티 리포지토리

---
+++ lib/core/domain/repository/community_repository.dart
@@ -0,0 +1,14 @@
+import 'package:cuteshrew/core/domain/entity/community_entity.dart';
+import 'package:cuteshrew/core/resources/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class CommunityRepository {
+  // 커뮤니티 정보 가져오기
+  Future<Either<Failure, CommunityEntity>> getCommunity({
+    required String communityName,
+  });
+
+  Future<Either<Failure, List<CommunityEntity>>> getCommunityList({
+    required String communityName,
+  });
+}
