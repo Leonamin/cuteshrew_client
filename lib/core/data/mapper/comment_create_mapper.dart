518f3e69295c3fae91df635d15ab6809fd053d61
Leonamin <yism810@gmail.com>
2022-12-24 22:58:38 +0900

    추가 - 댓글 생성 매퍼 추가

---
+++ lib/core/data/mapper/comment_create_mapper.dart
@@ -0,0 +1,11 @@
+import 'package:cuteshrew/core/data/dto/comment_create_dto.dart';
+import 'package:cuteshrew/core/data/mapper/mapper.dart';
+import 'package:cuteshrew/core/domain/entity/comment_create_entity.dart';
+
+class CommentCreateMapper
+    extends Mapper<CommentCreateEntity, CommentCreateDTO> {
+  @override
+  CommentCreateDTO map(CommentCreateEntity object) {
+    return CommentCreateDTO(comment: object.comment);
+  }
+}
