557e01d9dedca12615d99f46c1fdde5e27ed868b
Leonamin <yism810@gmail.com>
2022-12-24 21:42:29 +0900

    추가 - 로그인 토큰 매퍼 추가 변환, 역변환 기능

---
+++ lib/core/data/mapper/login_token_mapper.dart
@@ -0,0 +1,23 @@
+import 'package:cuteshrew/core/data/dto/login_token_dto.dart';
+import 'package:cuteshrew/core/data/mapper/mapper.dart';
+import 'package:cuteshrew/core/domain/entity/bearer_login_token.dart';
+import 'package:cuteshrew/core/domain/entity/login_token_entity.dart';
+
+class LoginTokenMapper extends Mapper<LoginTokenDTO, LoginTokenEntity> {
+  @override
+  LoginTokenEntity map(LoginTokenDTO object) {
+    return BearerLoginTokenEntity(
+      accessToken: object.accessToken,
+      tokenType: object.tokenType,
+      expires: object.expires ?? 0,
+    );
+  }
+
+  LoginTokenDTO toDTO(LoginTokenEntity object) {
+    return LoginTokenDTO(
+      accessToken: object.accessToken,
+      tokenType: object.tokenType,
+      expires: object.expires,
+    );
+  }
+}
