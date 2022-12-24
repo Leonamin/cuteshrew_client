82353b629804c50a4e5544ab0b59b662a7916028
Leonamin <yism810@gmail.com>
2022-12-24 17:55:46 +0900

    추가 - 로그인

--- lib/core/data/dto/login_token_dto.dart
+++ lib/core/data/dto/login_token_dto.dart
@@ -1,21 +1,25 @@
 class LoginTokenDTO {
   final String accessToken;
   final String tokenType;
+  final int? expires;
 
   const LoginTokenDTO({
     required this.accessToken,
     required this.tokenType,
+    this.expires,
   });
 
   factory LoginTokenDTO.fromJson(Map<String, dynamic> json) {
     return LoginTokenDTO(
       accessToken: json['access_token'],
       tokenType: json['token_type'],
+      expires: json['expires'],
     );
   }
 
   Map<String, dynamic> toJson() => {
         'access_token': accessToken,
         'token_type': tokenType,
+        'expires': expires,
       };
 }
