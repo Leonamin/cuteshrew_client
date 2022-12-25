4737fc52a4ac56385712549433b506a1ea4c5f9f
Leonamin <yism810@gmail.com>
2022-12-25 15:10:30 +0900

    변경 - 게시글 엔티티들 간의 구조 재정의

--- lib/core/domain/entity/posting_detail_entity.dart
+++ lib/core/domain/entity/posting_detail_entity.dart
@@ -2,16 +2,18 @@ import 'package:cuteshrew/core/domain/entity/community_entity.dart';
 import 'package:cuteshrew/core/domain/entity/posting_entity.dart';
 import 'package:cuteshrew/core/domain/entity/user_entity.dart';
 
-// 게시글 내용 없이 커뮤니티와 유저 정보를 넣는다.
+// 게시글의 내용을 포함해서 자세한 정보를 제공한다.
 // 2022-12-25
-// PostingEntity를 기본으로 두고 아래에 Preview를 두게 만들었다.
-class PostingPreviewEntity extends PostingEntity {
+// 최대한 UserEntity와 CommunityEntity의 변경사항에 다른 게시글 엔티티가 영향이 없도록 추상클래스를 분리시켜 상속받는다.
+class PostingDetailEntity extends PostingEntity {
+  final String body;
   final UserEntity writer;
   final CommunityEntity ownCommunity;
 
-  const PostingPreviewEntity({
+  const PostingDetailEntity({
     required int postId,
     required String title,
+    required this.body,
     required bool isLocked,
     required int publishedAt,
     required int updatedAt,
@@ -31,6 +33,7 @@ class PostingPreviewEntity extends PostingEntity {
   List<Object?> get props => [
         postId,
         title,
+        body,
         isLocked,
         publishedAt,
         updatedAt,
