095175b821ae90c2e7adbe7c8323a482b8466539
Leonamin <51023129+Leonamin@users.noreply.github.com>
2022-12-25 21:31:10 +0900

    추가 - 게시글 프로바이더 및 상태 추가 상태는 뷰모델로써 기능하도록 수정

---
+++ lib/presentation/screens/posting/providers/posting_page_provider.dart
@@ -0,0 +1,86 @@
+import 'package:cuteshrew/core/domain/entity/login_token_entity.dart';
+import 'package:cuteshrew/core/domain/entity/posting_entity.dart';
+import 'package:cuteshrew/core/domain/usecase/show_posting_page_usecase.dart';
+import 'package:cuteshrew/core/resources/failure.dart';
+import 'package:cuteshrew/presentation/screens/posting/providers/posting_page_state.dart';
+import 'package:flutter/widgets.dart';
+
+class PostingNotifier extends ValueNotifier<PostingPageState> {
+  late ShowPostingPageUseCase _postingPageUseCase;
+
+  PostingNotifier({
+    required int postId,
+    required String communityName,
+    required ShowPostingPageUseCase postingPageUseCase,
+  }) : super(PostingPageState.notLoaded(
+            postId: postId, communityName: communityName)) {
+    _postingPageUseCase = postingPageUseCase;
+  }
+  int get postId => value.postId;
+  String get communityName => value.communityName;
+
+  Future<void> getPosting([String? password]) async {
+    if (value is! LoadingPostingPageState) {
+      value = PostingPageState.loading(
+          postId: postId, communityName: communityName);
+
+      final result = await _postingPageUseCase(communityName, postId, password);
+
+      result.fold((Failure failure) {
+        //TODO 여기서 좀 작업 하자
+        // 비밀번호
+        value = PostingPageState.needPassword(
+          postId: postId,
+          communityName: communityName,
+        );
+        // 데이터 없음
+        // value = PostingPageState.noData(
+        //   postId: postId,
+        //   communityName: communityName,
+        // );
+        // 모르는 에러
+      }, (data) {
+        value = PostingPageState.loadedData(
+          postId: postId,
+          communityName: communityName,
+          postingEntity: data,
+        );
+      });
+    }
+  }
+
+  Future<void> setPosting(
+      String communityName, int postId, PostingEntity postingEntity) async {
+    value = PostingPageState.loadedData(
+      postId: postId,
+      communityName: communityName,
+      postingEntity: postingEntity,
+    );
+  }
+
+  Future<void> deletePosting(LoginTokenEntity loginToken) async {
+    if (value is LoadedDataPostingPageState) {
+      value = PostingPageState.loadingDelete(
+          postId: postId, communityName: communityName);
+
+      final result = await _postingPageUseCase.deletePosting(
+        communityName,
+        postId,
+        loginToken,
+      );
+
+      result.fold((Failure failure) {
+        value = PostingPageState.unknownError(
+          postId: postId,
+          communityName: communityName,
+          error: failure,
+        );
+      }, (data) {
+        value = PostingPageState.deletedData(
+          postId: postId,
+          communityName: communityName,
+        );
+      });
+    }
+  }
+}
