d5042c932e053d04b9946412f13d5322067e86c8
Leonamin <yism810@gmail.com>
2022-12-25 15:27:17 +0900

    변경 - 커뮤니티 매퍼 재정의

--- lib/core/data/mapper/community_mapper.dart
+++ lib/core/data/mapper/community_mapper.dart
@@ -1,11 +1,13 @@
 import 'package:cuteshrew/core/data/dto/community_dto.dart';
 import 'package:cuteshrew/core/data/mapper/mapper.dart';
+import 'package:cuteshrew/core/data/mapper/posting_preview_mapper.dart';
 import 'package:cuteshrew/core/domain/entity/community_entity.dart';
 import 'package:cuteshrew/core/domain/entity/community_preview_entity.dart';
 
 class CommunityMapper extends Mapper<CommunityDTO, CommunityEntity> {
   @override
   CommunityEntity map(CommunityDTO object) {
+    PostingPreviewMapper postingMapper = PostingPreviewMapper();
     /*
       기본처리
       communityName: 없음 처리
@@ -13,8 +15,10 @@ class CommunityMapper extends Mapper<CommunityDTO, CommunityEntity> {
       postingCount: 0개로 처리
      */
     return CommunityPreviewEntity(
-        communityName: object.communityName ?? "",
-        communityShowName: object.communityShowName ?? "",
-        postingCount: object.postingsCount ?? 0);
+      communityName: object.communityName ?? "",
+      communityShowName: object.communityShowName ?? "",
+      postingCount: object.postingsCount ?? 0,
+      postings: [for (final e in object.postings ?? []) postingMapper.map(e)],
+    );
   }
 }
