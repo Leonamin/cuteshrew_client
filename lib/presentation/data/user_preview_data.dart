96dd4ce33d3e33e4a4460bb05d5708f9a8419cdf
Leonamin <51023129+Leonamin@users.noreply.github.com>
2022-12-25 22:39:58 +0900

    수정 - 오타 수정

--- lib/presentation/data/user_preview_data.dart
+++ lib/presentation/data/user_preview_data.dart
@@ -1,7 +1,7 @@
-import 'package:cuteshrew/core/domain/entity/user_entity.dart';
+import 'package:cuteshrew/presentation/data/user_data.dart';
 
 // 기본 유저 정보를 상속 받고 최소 정보만 담긴 데이터
-class UserPreviewData extends UserEntity {
+class UserPreviewData extends UserData {
   const UserPreviewData({
     required String name,
     required String email,
