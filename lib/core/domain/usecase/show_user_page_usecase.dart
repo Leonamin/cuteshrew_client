529889ca138c0c7eef70d92e50ea17d18c1c57cf
Leonamin <51023129+Leonamin@users.noreply.github.com>
2022-12-27 20:03:06 +0900

    수정 - 도메인 변경 내용에 따른 유즈케이스 수정

--- lib/core/domain/usecase/show_user_page_usecase.dart
+++ lib/core/domain/usecase/show_user_page_usecase.dart
@@ -24,8 +24,14 @@ class ShowUserPageUsecase {
   }
 
   // 유저의 게시글들을 가져온다.
+  // userName은 필수
+  // startAtId는 순서에 따라 (지금은 순서를 정하는게 없으니 최신순) 지정 아이디부터 혹은 순서 처음부터(지금은 가장 최신부터)
+  // loadCount는 한번에 가져올 때 얼마나 가져올지 정하기
   Future<Either<Failure, List<PostingPreviewEntity>>> loadPostings(
-      String userName, int startAtId, int loadCount) {
+    String userName, [
+    int? startAtId,
+    int? loadCount,
+  ]) {
     return postingRepository.getPostingsByUser(
         userName: userName, startAtId: startAtId, loadCount: loadCount);
   }
@@ -33,7 +39,10 @@ class ShowUserPageUsecase {
   // 유저의 댓글들을 가져온다.
 
   Future<Either<Failure, List<CommentDetailEntity>>> loadComments(
-      String userName, int startAtId, int loadCount) {
+    String userName, [
+    int? startAtId,
+    int? loadCount,
+  ]) {
     return commentRepository.getCommentsByUser(
         userName: userName, startAtId: startAtId, loadCount: loadCount);
   }
