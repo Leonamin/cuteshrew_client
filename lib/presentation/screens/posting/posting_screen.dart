7706a93ee60303aefa694b47921b08e37bbb04ed
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-05 22:37:23 +0900

    추가 - 링크 기능 추가

--- lib/presentation/screens/posting/posting_screen.dart
+++ lib/presentation/screens/posting/posting_screen.dart
@@ -15,7 +15,9 @@ import 'package:cuteshrew/presentation/strings/strings.dart';
 import 'package:cuteshrew/presentation/widgets/common_widgets/clickable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PostingScreen extends StatefulWidget {
   final String communityName;
@@ -291,10 +293,19 @@ class LoadedDataPostingPageScreen extends StatelessWidget {
             height: 8,
           ),
           // SelectableHtml 쓰면 이미지가 출력이 안된다.
-          Html(
-            data: postingPageState.content,
-            onLinkTap: (url, context, attributes, element) {},
+          HtmlWidget(
+            postingPageState.content,
+            onTapUrl: (url) {
+              launchUrl(Uri.parse(url));
+              return true;
+            },
+            // TODO Selectable한 텍스트를 만들고 싶으면 속성별로 나누는 빌더를 만들어야할 것 같다.
+            // return void; customWidget이 아닌 원래 내보내는 위젯을 반환하고 return FooWidget()을 하면 딱 FooWidget()이 나온다.
+            customWidgetBuilder: (element) {
+              return;
+            },
           ),
+          // Html(data: postingPageState.content),
           const Divider(
             height: 8,
           ),
