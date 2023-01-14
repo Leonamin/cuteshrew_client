d07e2345558f2059051304c1908dae4393b6301b
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-14 16:56:07 +0900

    추가 - 에러페이지에 메인화면으로 돌아가기

--- lib/presentation/screens/notfound/error_page.dart
+++ lib/presentation/screens/notfound/error_page.dart
@@ -1,3 +1,6 @@
+import 'package:cuteshrew/di/navigation_service.dart';
+import 'package:cuteshrew/di/service_locator.dart';
+import 'package:cuteshrew/presentation/config/route/routes.dart';
 import 'package:cuteshrew/presentation/widgets/common_widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
@@ -31,7 +34,9 @@ class PageNotFound extends StatelessWidget {
             height: 16,
           ),
           OutlinedButton(
-            onPressed: () {},
+            onPressed: () {
+              locator<NavigationService>().navigateTo(Routes.HomePageRoute);
+            },
             // FIXME 하드코딩 언어
             child: const CustomText(
               text: "메인 페이지로 가기",
