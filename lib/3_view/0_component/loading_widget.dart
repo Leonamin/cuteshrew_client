5d216bbba87486c57a8edc0a25e802ee67b51f36
Leonamin <yism810@gmail.com>
2024-03-26 22:15:08 +0900

    # ADD - 로딩 위젯 아바타 글로우 효과

--- lib/3_view/0_component/loading_widget.dart
+++ lib/3_view/0_component/loading_widget.dart
@@ -1,3 +1,6 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:cuteshrew/0_foundation/asset/asset_image_icon.dart';
+import 'package:cuteshrew/0_foundation/ui/color_palettes.dart';
 import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
@@ -5,6 +8,19 @@ class LoadingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
+    return Center(
+      child: SizedBox(
+        height: 100,
+        width: 100,
+        child: AvatarGlow(
+          glowColor: ColorPaletts.primary1,
+          duration: const Duration(milliseconds: 1000),
+          child: Image.asset(
+            AssetImageIcon.logo.path,
+            height: 50,
+          ),
+        ),
+      ),
+    );
   }
 }
